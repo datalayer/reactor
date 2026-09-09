c8506b71ae37d394a3b16781507e5ca0e0f3ccae
Eric Charles <eric@datalayer.io>
2026-09-09 03:36:32 +0200

    docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -197,11 +197,6 @@ module.exports = {
           position: 'left',
         },
         {
-          to: '/federation/',
-          label: 'Federation',
-          position: 'left',
-        },
-        {
           href: 'https://discord.gg/YQFwvmSSuR',
           position: 'right',
           className: 'header-discord-link',
